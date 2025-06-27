16c305f91a221cbda8431104b312df00994c10de
Bruno-Bertol-TI <bbertol2312@outlook.com>
2025-06-27 19:04:22 -0300

    feat: create ClienteSection component with styles and functionality

---
+++ src/Components/ClientSection/index.js
@@ -0,0 +1,13 @@
+import BannerCarousel from '../BannerCarousel'
+import SectionHeader from '../SectionHeader'
+import './ClientSection.css'
+
+const ClientSection = () => {
+    return (
+        <section className='section-client'>
+            <BannerCarousel/>
+        </section>
+    )
+}
+
+export default ClientSection
